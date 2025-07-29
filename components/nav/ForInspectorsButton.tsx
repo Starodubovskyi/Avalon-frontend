ba469ca40d935b99834c13b94db311066681294d
Maksym Kokosh <mkokosh03@gmail.com>
2025-07-29 15:03:46 +0300

    Fit NaveBar and MainPage

--- components/nav/ForInspectorsButton.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-const ForInspectorsButton = () => (
-  <Link href="#for-inspectors" className="nav-button">
-    For Inspectors
-  </Link>
-);
-
-export default ForInspectorsButton;
--- components/nav/ForOwnersButton.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-const ForOwnersButton = () => (
-  <Link href="#for-ship-owners" className="nav-button">
-    For Ship Owners
-  </Link>
-);
-
-export default ForOwnersButton;
