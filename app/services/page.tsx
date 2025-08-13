af459e294a71d635ede14cad63af15467f71dc17
Bogdan060 <rumubogdan@gmail.com>
2025-08-13 15:07:11 +0300

    I added a My Online Services section with tabs, and I will decide later what functionality to assign to them.

---
+++ app/services/page.tsx
@@ -0,0 +1,11 @@
+import TopProfileNavbar from "@/components/profile/TopProfileNavbar";
+import MyOnlineServices from "@/components/onlineServices/MyOnlineServices";
+
+export default function ServicesPage() {
+  return (
+    <div className="w-full">
+      <TopProfileNavbar />
+      <MyOnlineServices />
+    </div>
+  );
+}
